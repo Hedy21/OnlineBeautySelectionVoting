f519b18673e92a405c7990e802024621efb12e06
PyronSu <tspaing699@gmail.com>
2024-01-09 19:41:04 +0630

    save register data to DB with hashed password but you can paste the password from other which I need to fix

--- app/Http/Controllers/VoterController.php
+++ app/Http/Controllers/VoterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Voter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class VoterController extends Controller
@@ -22,6 +24,7 @@ class VoterController extends Controller
 
     public function register(Request $request){
         //dd($request->all());
+        // validation start
         $validationRulesRegister = [
             'email' => 'required|email|ends_with:ucstgi.edu.mm|unique:voters,email',
             'password' => 'required|min:6|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/',
@@ -41,6 +44,25 @@ class VoterController extends Controller
         if($validator->fails()){
             return redirect()->back()->withErrors($validator)->withInput();
         }
+        // validation end
+
+
+
+        // create in DB start
+        $data = $this->getRegisterData($request);
+        Voter::create($data);
+        // create in DB end
         return view('leaderboard');
+    }
+
+    private function getRegisterData($request){
+        // Hash the password before saving to the database
+        $hashedPassword = Hash::make($request->input('password'));
+
+        $data = [
+            'email' => $request->email,
+            'password' => $hashedPassword
+        ];
+        return $data;
     }
 }
